9d76b1b151237357153830989468ed105c317c13
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-07-10 11:23:08 +0100

    New Post, Index, ReadBar

---
+++ HTML/Posts/Layout/barraDeLeitura.js
@@ -0,0 +1,18 @@
+let readBar = document.querySelector('.readBar')
+readBar.style.width = '0%'
+let endPoint = document.querySelector('#endOfPost')
+let offSetTop = endPoint.offsetTop - 450;
+let sizeOfReadBar = 0;
+
+
+window.onscroll = () => {
+    sizeOfReadBar = readBar.getAttribute('style').split(' ')[1].replace(';', '').replace('%', '')
+    if(window.scrollY > 1 || window.scrollY < offSetTop && sizeOfReadBar < 100){
+        result = (window.scrollY * 100) / offSetTop;
+        result = result.toFixed(2)
+        readBar.style.width = `${result}%`
+        console.log(sizeOfReadBar)
+    } else if(window.scrollY == 0) {
+        readBar.style.width = "0"
+    }
+}
