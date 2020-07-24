15bc0f891cf0a330743f1790a6f37a222a12ce22
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-07-24 13:30:26 +0100

    Novo Layout Desktop do site

--- HTML/Posts/Layout/indice.js
+++ HTML/Posts/Layout/indice.js
@@ -3,13 +3,13 @@ let flag = false
 document.getElementById('buttonIndice').onclick = function (event){
     if(!flag){
         document.getElementById('divIndice').style.display = 'block'
-        event.target.style.color = '#002238'
+        event.target.style.color = '#2ea6d2'
         event.target.style.backgroundColor = '#fff'
         flag = true;
     } else {
         document.getElementById('divIndice').style.display = 'none'
         event.target.style.color = '#fff'
-        event.target.style.backgroundColor = '#002238'
+        event.target.style.backgroundColor = '#2ea6d2'
         flag = false;
     }
 }
